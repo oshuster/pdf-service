cb34cfdafbb50fe1642831180edd7c63a1f36e5c
oshuster <shuster.oleksandr@gmail.com>
2024-09-23 00:18:21 +0300

    [+] add routing

--- routes/pdfServiceRouter.js
+++ routes/pdfServiceRouter.js
@@ -2,11 +2,14 @@ import express from "express"
 
 import { ctrlWrapper } from "../helpers/ctrlWrapper.js"
 import { logRequest } from "../config/logConfig.js"
+import { makePdf } from "../controllers/pdfController.js"
 
 const pdfServiceRouter = express.Router()
 
 pdfServiceRouter.use(logRequest)
 
-// pdfServiceRouter.get("/make", ctrlWrapper())
+// TODO: add midlleware for validation body
+
+pdfServiceRouter.post("/make", ctrlWrapper(makePdf))
 
 export default pdfServiceRouter
