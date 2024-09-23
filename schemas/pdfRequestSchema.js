c1459df979fff939ab36635e7c4390c69e8baf8c
oshuster <godzilasanya@gmail.com>
2024-09-23 17:23:36 +0300

    [+] added initial schema

---
+++ schemas/pdfRequestSchema.js
@@ -0,0 +1,7 @@
+import Joi from "joi";
+
+export const pdfRequestSchema = Joi.object({
+  docName: Joi.string().required(),
+  html: Joi.string().required(),
+  styles: Joi.string().optional(),
+});
