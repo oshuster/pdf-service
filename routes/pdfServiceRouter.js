fd0acd1307a65717b20934d598c803b73deeb57d
oshuster <shuster.oleksandr@gmail.com>
2024-09-23 22:36:28 +0300

    [+] added midleware for validate request and markup

--- routes/pdfServiceRouter.js
+++ routes/pdfServiceRouter.js
@@ -3,13 +3,19 @@ import express from "express"
 import { ctrlWrapper } from "../helpers/ctrlWrapper.js"
 import { logRequest } from "../config/logConfig.js"
 import { makePdf } from "../controllers/pdfController.js"
+import validateBody from "../helpers/validateBody.js"
+import { pdfRequestSchema } from "../schemas/pdfRequestSchema.js"
+import validateHtmlSyntax from "../helpers/validateHtml.js"
 
 const pdfServiceRouter = express.Router()
 
 pdfServiceRouter.use(logRequest)
 
-// TODO: add midlleware for validation body
-
-pdfServiceRouter.post("/make", ctrlWrapper(makePdf))
+pdfServiceRouter.post(
+  "/make",
+  validateBody(pdfRequestSchema),
+  validateHtmlSyntax,
+  ctrlWrapper(makePdf)
+)
 
 export default pdfServiceRouter
