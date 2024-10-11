5a8d0cb9bc9aa16dc415b0f14748f15d399cdce3
oshuster <shuster.oleksandr@gmail.com>
2024-10-11 12:19:30 +0300

    [!] change schema

--- schemas/pdfRequestSchema.js
+++ schemas/pdfRequestSchema.js
@@ -3,5 +3,6 @@ import Joi from "joi";
 export const pdfRequestSchema = Joi.object({
   docName: Joi.array().items(Joi.string()).min(1).required(),
   landscape: Joi.boolean(),
+  zip: Joi.boolean(),
   html: Joi.string().required(),
 });
