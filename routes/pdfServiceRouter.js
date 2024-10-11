8ca69a69d9e2ff260b5170a9c7cb7f10187d050e
oshuster <shuster.oleksandr@gmail.com>
2024-10-11 12:21:17 +0300

    [*] change routing
    moved for 2 main route(reports and all files)
    added description for swagger

--- routes/pdfServiceRouter.js
+++ routes/pdfServiceRouter.js
@@ -1,25 +1,35 @@
 import express from "express";
 import { ctrlWrapper } from "../helpers/ctrlWrapper.js";
-import { logRequest } from "../config/logConfig.js";
-import { pdfController } from "../controllers/pdfController.js";
 import { pdfRequestSchema } from "../schemas/pdfRequestSchema.js";
 import { validatorBody } from "../middlewares/validatorBody.js";
 import { validatorHtmlSyntax } from "../middlewares/validatorHtmlSyntax.js";
 import { addUuidMiddleware } from "../middlewares/addUuidMiddleware.js";
-import { zipController } from "../controllers/zipController.js";
-import { actController } from "../controllers/actController.js";
-import { actInZipController } from "../controllers/actInZipController.js";
+import { pdfDocumentController } from "../controllers/pdfDocumentsController.js";
+import { pdfController } from "../controllers/pdfController.js";
 
 const pdfServiceRouter = express.Router();
 
-pdfServiceRouter.post(
-  "/make-zip",
-  validatorBody(pdfRequestSchema),
-  validatorHtmlSyntax,
-  addUuidMiddleware,
-  ctrlWrapper(zipController)
-);
-
+/**
+ * @swagger
+ * /make-pdf:
+ *   post:
+ *     summary: Генерація PDF документів
+ *     description: Генерує PDF документ на основі даних, переданих у запиті.
+ *     tags: [PDF Generation]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/PdfRequest'
+ *     responses:
+ *       200:
+ *         description: Успішна генерація PDF.
+ *       400:
+ *         description: Помилка валідації даних.
+ *       500:
+ *         description: Внутрішня помилка сервера.
+ */
 pdfServiceRouter.post(
   "/make-pdf",
   validatorBody(pdfRequestSchema),
@@ -28,20 +38,33 @@ pdfServiceRouter.post(
   ctrlWrapper(pdfController)
 );
 
+/**
+ * @swagger
+ * /doc-pdf:
+ *   post:
+ *     summary: Генерація PDF для ЗВІТІВ
+ *     description: Генерує PDF для документів на основі переданих даних.
+ *     tags: [PDF Generation]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/PdfRequest'
+ *     responses:
+ *       200:
+ *         description: Успішна генерація PDF для документу.
+ *       400:
+ *         description: Помилка валідації даних.
+ *       500:
+ *         description: Внутрішня помилка сервера.
+ */
 pdfServiceRouter.post(
-  "/act-pdf",
+  "/doc-pdf",
   validatorBody(pdfRequestSchema),
   validatorHtmlSyntax,
   addUuidMiddleware,
-  ctrlWrapper(actController)
-);
-
-pdfServiceRouter.post(
-  "/act-zip",
-  validatorBody(pdfRequestSchema),
-  validatorHtmlSyntax,
-  addUuidMiddleware,
-  ctrlWrapper(actInZipController)
+  ctrlWrapper(pdfDocumentController)
 );
 
 export default pdfServiceRouter;
