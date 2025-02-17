b524579c207a0b9c0e1a9444b44b85e07c1f2a74
oshuster <shuster.oleksandr@gmail.com>
2025-02-18 00:20:43 +0200

    [*] fix fileName logged

--- src/helpers/sendBase64.js
+++ src/helpers/sendBase64.js
@@ -1,8 +1,13 @@
-import { serviceLogger } from "../config/logConfig.js";
+import { serviceLogger } from '../config/logConfig.js';
 
 export const sendPdfAsBase64 = async (req, res, pdfBuffer) => {
-  const base64Pdf = pdfBuffer.toString("base64");
+  const fileName =
+    req.body.docType !== undefined
+      ? req.body.docType
+      : req.body.docName || 'document';
+
+  const base64Pdf = pdfBuffer.toString('base64');
   res.status(200).json({ pdfBase64: base64Pdf });
 
-  serviceLogger.info(`PDF created and sent for act: ${req.body.docType}`);
+  serviceLogger.info(`PDF created and sent for act: ${fileName}`);
 };
