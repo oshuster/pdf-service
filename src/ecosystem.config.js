c409ec22e9516a2757638c37722ae1f58047efdd
oshuster <shuster.oleksandr@gmail.com>
2025-02-19 21:58:56 +0200

    [+] added cluster config

---
+++ src/ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'pdf-service',
+      script: 'src/app.js',
+      exec_mode: 'cluster',
+      instances: 4,
+      autorestart: true,
+      watch: ['styles/all-pdf-styles', 'styles/documents'],
+      ignore_watch: ['node_modules', 'logs', 'output', 'styles_bak'],
+      watch_delay: 5000,
+    },
+  ],
+};
